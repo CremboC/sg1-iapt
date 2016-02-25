61f1eba4c1517815b124a8e04756e3b952c5bf23
cremboc <dev@crembo.eu>
2016-02-25 22:55:10 +0000

    Search WIP Committing because VM decided to absolutely fuck up and I'm scared everything will disappear

--- controllers/search.py
+++ controllers/search.py
@@ -1,4 +1,18 @@
 def index():
+    query_string = request.vars.query
+    filtered = request.vars.filtered == "true" or False
 
-    return dict()
+    # fill filters
+    types = db(db.types.id > 0).select()
 
+    search_query = db.objects.name.contains(query_string)  # search by the query
+    search_query &= db.objects.type_id == db.types.id  # join type info
+    search_query &= db.objects.owner_id == db.auth_user.id  # join user info
+
+    if filtered:
+        if request.vars.types:
+            search_query &= db.objects.type_id.belongs(request.vars.types)
+
+    results = db(search_query).select()
+
+    return dict(results=results, types=types, filtered=filtered)
