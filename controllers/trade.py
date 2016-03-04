47360c606c91c875b0bf7a8945ba8612e7ba68c2
Miguel Boland <miguelboland@hotmail.com>
2016-03-04 09:30:57 +0000

    Refactored page names, added icons to trade home, bug fixes

--- controllers/trade.py
+++ controllers/trade.py
@@ -1,8 +1,8 @@
-# note: don't trade wanted items
+import datetime
 
-@auth.requires_login()
-def index():
-    print 'a'
+
+# todo: don't trade wanted items
+
 
 
 @auth.requires_login()
@@ -50,7 +50,7 @@ def view():
 
 
     # Verify whether trade is editable
-    editable = (trade.status == 0)
+    editable = (trade.status == 0) | (trade.status == 4)
     if trader_username is None:
         trader_username = db(db.auth_user.id == trader_id).select(db.auth_user.username).column()[0]
     return {"user_is_receiver": auth.user_id == trade.receiver, "tradeid": tradeid, "trader_id": trader_id,
@@ -88,11 +88,12 @@ def edit():
     if trade is None:
         raise HTTP(404, "Error 404: Invalid request, trade does not exist")
 
+    if (trade.status >= 1) & (trade.status <= 3):
+        raise HTTP(403, "Error 403: Trade has already been completed")
     # or user is not permitted to view
     if (auth.user_id != trade.sender) & (auth.user_id != trade.receiver):
         raise HTTP(404, "Error 404: Invalid request, trade does not exist")
 
-    # Todo check trade is not completed, if so redirect to view
     # Get objects currently in trade from sender
     trade_objects2 = db(
         (db.trades_sending.trade_id == tradeid) & (db.trades_sending.sent_object_id == db.objects.id)).select(
@@ -120,16 +121,13 @@ def edit():
         trader_id = trade.sender
         trade_objects = [trade_objects1, trade_objects2]
 
-
-
     # Verify whether trade is editable
     editable = (trade.status == 0)
     if trader_username is None:
         trader_username = db(db.auth_user.id == trader_id).select(db.auth_user.username).column()[0]
-    print trade_objects[1]
 
-    print get_available_user_items(trade.receiver)
-    return {"user_id": auth.user_id, "trader_id": trader_id, "status": trade.status, "trader_username": trader_username,
+    return {"prevtrade": tradeid, "user_id": auth.user_id, "trader_id": trader_id, "status": trade.status,
+            "trader_username": trader_username,
             "editable": editable,
             "trade_objects": trade_objects,
             "available_objects": [get_available_user_items(auth.user_id), get_available_user_items(trader_id)]}
@@ -137,10 +135,10 @@ def edit():
 
 @auth.requires_login()
 def createNew():
-    print request.vars
+
     if (request.vars['receiverObjects'] is None) | (request.vars['senderObjects'] is None):
         response.status = 400
-        return 'Error: incomplete trade, please enter trade items or select other user'
+        return 'Error 400: incomplete trade, please enter trade items or select other user'
 
     senderId = int(auth.user_id)
     # Check user owns all objects he proposes
@@ -150,39 +148,45 @@ def createNew():
     # Verify all objects belong to correct user
     senderobjectIds = map(int, request.vars['senderObjects'])
     senderobjects = db(db.objects.id.belongs(senderobjectIds)).select(db.objects.owner_id).column()
+
     for string in senderobjects:
         if string != senderId:
             response.status = 400
-            return 'Error: invalid item ID, please refresh page'
+            return 'Error 400: invalid item ID, please refresh page'
 
-    receivedobjectIds = map(int, request.vars['receiverObjects'])
-    receivedobjects = db(db.objects.id.belongs(receivedobjectIds)).select(db.objects.owner_id).column()
-    receiverId = receivedobjects[0].id
+    receiverobjectIds = map(int, request.vars['receiverObjects'])
+    receiverobjects = db(db.objects.id.belongs(receiverobjectIds)).select(db.objects.owner_id).column()
 
-    for string in receivedobjects:
+
+    receiverId = receiverobjects[0].id
+
+    for string in receiverobjects:
         if string != receiverId:
             response.status = 400
-            return 'Error: invalid item ID, please refresh page'
+            return 'Error 400: invalid item ID, please refresh page'
 
-    newTradeId = db.trades.insert(sender=senderId, receiver=receiverId, status=0, seen=False)
-    # Link to previous trade
-    if request.vars.prevTrade is not None:
-        db(db.trades.id == int(request.vars.prevTrade)).update(superseded_by=newTradeId, status=4)
+    if request.vars.prevtrade is not None:
+        newTradeId = db.trades.insert(sender=senderId, receiver=receiverId, status=4, seen=False)
+    else:
+        newTradeId = db.trades.insert(sender=senderId, receiver=receiverId, status=0, seen=False)
 
     for itemId in senderobjectIds:
         db.trades_sending.insert(trade_id=newTradeId, sent_object_id=itemId)
-    for itemId in receivedobjectIds:
+    for itemId in receiverobjectIds:
         db.trades_receiving.insert(trade_id=newTradeId, recv_object_id=itemId)
     receiver_username = db(db.auth_user.id == receiverId).select(db.auth_user.username).column()[0]
-    session.flash = T(
-        'Successfully created trade with ' + receiver_username if receiver_username is not None else 'unknown')
-    redirect(URL('trade', 'home'), client_side=True)
+    session.flash = T('Successfully created trade with ' + receiver_username)
+
+    if request.vars.prevtrade is not None:
+        db(db.trades.id == int(request.vars.prevtrade)).update(status=4, superseded_by=newTradeId)
+
+    redirect(URL('trade', 'index'), client_side=True)
 
 
 @auth.requires_login()
 def accept():
     tradeid = request.vars.tradeid
-    if (tradeid is None) | (type(tradeid) is not (int | long)):
+    if (tradeid is None) | (not isinstance(tradeid, str)):
         raise HTTP(400, "Error 400: Trade id invalid or does not exist")
     trade = db.trades[tradeid]
     if trade is None:
@@ -201,6 +205,8 @@ def accept():
 
     # Mark as Accepted
     db(db.trades.id == tradeid).update(status=3)
+    session.flash = T("Trade was accepted")
+    redirect(URL('trade', 'index'), client_side=True)
 
 
 @auth.requires_login()
@@ -219,13 +225,17 @@ def delete():
 
     # Mark as rejected or cancelled
     if auth.user_id == trade.sender:
+        session.flash = T("Trade was cancelled")
         db(db.trades.id == tradeid).update(status=1)
     else:
+        session.flash = T("Trade was deleted")
         db(db.trades.id == tradeid).update(status=2)
-    redirect(URL('trade', 'home'))
+
+    redirect(URL('trade', 'index'), client_side=True)
+
 
 @auth.requires_login()
-def history():
+def index():
     trades = db((((db.trades.sender == auth.user_id) & (db.auth_user.id == db.trades.receiver)) | (
         (db.trades.receiver == auth.user_id) & (db.auth_user.id == db.trades.sender)))
                 ).select(db.trades.id, db.trades.date_created, db.trades.sender, db.trades.receiver, db.trades.status,
@@ -245,6 +255,8 @@ def history():
         receivedItems = db(db.trades_receiving.trade_id == trade.trades.id).count()
         recvItemValue = db((db.trades_receiving.trade_id == trade.trades.id) & (
             db.trades.id == db.trades_receiving.recv_object_id)).select(db.objects.currency_value).column()
+
+
         trade.value = sum(sentItemValue) + sum(recvItemValue)
 
         if trade.trades.sender != auth.user_id:
@@ -254,16 +266,24 @@ def history():
         trade.sentItems = sentItems
         trade.receivedItems = receivedItems
         trade.trades.status = int(trade.trades.status)
-        if (trade.trades.status == 1) | (trade.trades.status == 2) | (trade.trades.status == 3):
+
+        if trade.trades.superseded_by is not None:
+            completed.append(trade)
+        elif (trade.trades.status == 1) | (trade.trades.status == 2) | (trade.trades.status == 3):
             completed.append(trade)
         elif trade.trades.sender == auth.user_id:
             sent.append(trade)
         else:
             incoming.append(trade)
-    print incoming
-    print sent
-    print completed
-    return {"incoming": incoming, "sent": sent, "completed": completed}
+
+        if trade.trades.sender == auth.user_id:
+            trade.trades.otheruser = trade.trades.receiver
+        else:
+            trade.trades.otheruser = trade.trades.sender
+        trade.trades.otheruser = db(db.auth_user.id == trade.trades.otheruser).select(db.auth_user.username).column()[0]
+    completed.sort(key=lambda x: x.trades.date_created, reverse=True)
+
+    return {"incoming": incoming, "sent": sent, "completed": completed[:5], "user_id": auth.user_id}
 
 
 # Get items that are not currently in any trade
