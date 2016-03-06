e3b57ecd387af4362be0adeadd820fbc427d0d5a
crembo <cremboc@gmail.com>
2016-03-06 15:33:13 +0000

    Item create (with add to collections) and super basic view

--- controllers/items.py
+++ controllers/items.py
@@ -2,18 +2,26 @@
 def create():
     form = SQLFORM(db.objects)
     types = db(db.types.id > 0).select()
+    collections = db(db.collections.owner_id == auth.user_id).select()
 
     if form.process().accepted:
-        response.flash = {"status": "success", "message": "Item successfully saved"}
+
+        chosen_cols = [int(col) for col in request.vars.collections] or [request.vars.collections]
+
+        for col in chosen_cols:
+            db.object_collection.insert(object_id=form.vars.id, collection_id=col)
+
+        session.flash = {"status": "success", "message": "Item successfully saved"}
+        return redirect(URL('items', 'show', args=form.vars.id))
     elif form.errors:
         response.flash = {"status": "danger", "message": "An error has occured. See below."}
 
-    return dict(form=form, types=types)
+    return dict(form=form, types=types, collections=collections)
 
 
 @auth.requires_login()
 def show():
-    item_id = request.vars[0] or redirect(URL('default', 'index'))
+    item_id = request.args[0] or redirect(URL('default', 'index'))
     item = db(db.objects.id == item_id).select().first()
 
     return dict(item=item)
