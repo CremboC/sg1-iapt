86e4a89538b8a2f135e7ee8acdabd0522d1f4106
Miguel Boland <miguelboland@hotmail.com>
2016-03-08 15:50:34 +0000

    Refactored both views into 1

--- controllers/item_lists.py
+++ controllers/item_lists.py
@@ -1,32 +1,23 @@
-def have():
+def view():
+    want = request.vars.want == 'true'
+    print want
     user_id = request.vars.user_id
     if user_id is None:
         user_id = auth.user_id
     if user_id is None:
-        redirect(URL('default', 'user/login', vars={'_next':URL('item_lists', 'have')}))
+        if want:
+            redirect(URL('default', 'user/login', vars={'_next':URL('item_lists', 'want')}))
+        else:
+            redirect(URL('default', 'user/login', vars={'_next':URL('item_lists', 'have')}))
 
     username = db(db.auth_user.id == user_id).select(db.auth_user.username).column()
     if len(username) == 0:
         raise HTTP(404, "Error 404: Invalid request, userid does not exist")
     else:
         username = username[0]
-    have_items = db((db.objects.owner_id == auth.user_id) & (db.objects.status == 1)).select()
-
-    return {"is_owner": user_id == auth.user_id, "user_id": user_id, "username": username, "items": have_items}
-
-
-def want():
-    user_id = request.vars.user_id
-    if user_id is None:
-        user_id = auth.user_id
-    if user_id is None:
-        redirect(URL('default', 'user/login', vars={'_next':URL('item_lists', 'want')}))
-
-    username = db(db.auth_user.id == user_id).select(db.auth_user.username).column()
-    if len(username) == 0:
-        raise HTTP(404, "Error 404: Invalid request, userid does not exist")
+    if want:
+        items = db((db.objects.owner_id == auth.user_id) & (db.objects.status == 0)).select()
     else:
-        username = username[0]
-    want_items = db((db.objects.owner_id == auth.user_id) & (db.objects.status == 0)).select()
-
-    return {"is_owner": user_id == auth.user_id, "user_id": user_id, "username": username, "items": want_items}
+        items = db((db.objects.owner_id == auth.user_id) & (db.objects.status == 1)).select()
+    return {"is_want": want,
+            "user_id": user_id, "username": username, "items": items}
