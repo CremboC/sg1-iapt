b1795f95fee3a36b4f827a468b5fd7eb2e908543
Miguel Boland <miguelboland@hotmail.com>
2016-03-09 12:50:12 +0000

    Fixed broken links in layout

--- controllers/item_lists.py
+++ controllers/item_lists.py
@@ -1,6 +1,5 @@
 def view():
     want = request.vars.want == 'true'
-    print want
     user_id = request.vars.user_id
     if user_id is None:
         user_id = auth.user_id
@@ -19,5 +18,6 @@ def view():
         items = db((db.objects.owner_id == auth.user_id) & (db.objects.status == 0)).select()
     else:
         items = db((db.objects.owner_id == auth.user_id) & (db.objects.status == 1)).select()
+    # print items
     return {"is_want": want,
             "user_id": user_id, "username": username, "items": items}
