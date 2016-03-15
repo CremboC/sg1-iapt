a1aee32eaba85735284a726a6c90e5f1b979b2d6
Miguel Boland <miguelboland@hotmail.com>
2016-03-15 18:33:25 +0000

    Added trade value to the trade view page, began implementing item preview on upload

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,18 @@
 $(function () {
     $('[data-toggle="tooltip"]').tooltip();
 });
+
+$(document).on('change', '.input-file', function() {
+        if (this.files && this.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                $("#preview").css({'background-image': 'url('+ e.target.result+')'});
+            };
+            reader.readAsDataURL(this.files[0]);
+        }
+    });
+
+$(document).on('click', '.input-file', function() {
+    $("#preview").css({'background-image': 'url()'});
+    });
