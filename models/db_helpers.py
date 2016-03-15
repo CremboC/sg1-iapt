309846ac043ba4526e69db3849d6b96870722f19
Miguel Boland <miguelboland@hotmail.com>
2016-03-15 10:12:08 +0000

    Merged helper files

--- models/db_helpers.py
+++ models/db_helpers.py
@@ -43,3 +43,13 @@ def maybe_list(var):
         return [int(v) for v in var]
     else:
         return [int(var)]
+
+
+def add_in_trade_field(object):
+
+    in_trade_query = (((db.trades_receiving.recv_object_id == object.id) & (db.trades_receiving.trade_id == db.trades.id))
+                          | ((db.trades_sending.sent_object_id == object.id) & (db.trades_sending.trade_id == db.trades.id))) \
+                         & ~(db.trades.status.belongs([1,2,3]))
+    object.in_trade = len(db(in_trade_query).select()) > 0
+    return object
+
--- models/helper.py
+++
@@ -1,8 +0,0 @@
-def add_in_trade_field(object):
-
-    in_trade_query = (((db.trades_receiving.recv_object_id == object.id) & (db.trades_receiving.trade_id == db.trades.id))
-                          | ((db.trades_sending.sent_object_id == object.id) & (db.trades_sending.trade_id == db.trades.id))) \
-                         & ~(db.trades.status.belongs([1,2,3]))
-    object.in_trade = len(db(in_trade_query).select()) > 0
-    return object
-
