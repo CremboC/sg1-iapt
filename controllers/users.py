b2956bfc6b570eb35a540a38dc65ca2a7f1a61db
Miguel Boland <mb1069@york.ac.uk>
2016-04-18 18:20:17 +0100

    Fixed error on non-logged in index page

--- controllers/users.py
+++ controllers/users.py
@@ -1,7 +1,7 @@
 # Returns users for auto-complete feature
 def index():
     query = db.auth_user.id > 0
-    if request.vars.ignore:
+    if auth.is_logged_in():
         query = db.auth_user.id != int(request.vars.ignore)
 
     return dict(users=db(query).select(db.auth_user.id, db.auth_user.username))
